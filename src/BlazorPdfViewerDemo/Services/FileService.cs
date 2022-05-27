1bc407d38435dff7a140aade3f28378c4c442501
germandefrancesco <germandefrancesco@hotmail.com>
2022-05-27 12:51:07 -0300

    fix warnings

--- src/BlazorPdfViewerDemo/Services/FileService.cs
+++ src/BlazorPdfViewerDemo/Services/FileService.cs
@@ -1,44 +1,49 @@
 ﻿using BlazorPdfViewerDemo.Data;
 using Microsoft.AspNetCore.Components.Forms;
 
-namespace BlazorPdfViewerDemo.Services
+namespace BlazorPdfViewerDemo.Services;
+
+public interface IFileService
 {
-	public class FileService : IFileService
-	{
-		Microsoft.AspNetCore.Hosting.IHostingEnvironment _hostingEnvironment;
+	List<FileClass> GetAllPDFs();
+	Task Upload(IBrowserFile file);
+}
 
-		public FileService(Microsoft.AspNetCore.Hosting.IHostingEnvironment hostingEnvironment)
-		{
-			_hostingEnvironment = hostingEnvironment;
-		}
+public class FileService : IFileService
+{
+	IWebHostEnvironment _webHostEnvironment;
 
-		public List<FileClass> GetAllPDFs()
-		{
-			var files = new List<FileClass>();
-			string path = $"{_hostingEnvironment.WebRootPath}\\files\\";
-			var nFileId = 1;
-			foreach (var pdfPath in Directory.EnumerateFiles(path, "*.pdf"))
-			{
-				files.Add(new FileClass()
-				{
-					FileId = nFileId++,
-					Name = Path.GetFileName(pdfPath),
-					Path = pdfPath,
-				});
-			}
-			return files;
-		}
-
-        public async Task Upload(IBrowserFile file)
-        {
-			var path = Path.Combine(_hostingEnvironment.WebRootPath, "files", file.Name);
-			var memoryStream = new MemoryStream();
-			await file.OpenReadStream(10485760).CopyToAsync(memoryStream);
-			using (var fileStream = new FileStream(path, FileMode.Create, FileAccess.Write))
-			{
-				memoryStream.WriteTo(fileStream);
-			}
-			memoryStream.Close();
-		}
+    public FileService(IWebHostEnvironment webHostEnvironment)
+    {
+        _webHostEnvironment = webHostEnvironment;
     }
+
+    public List<FileClass> GetAllPDFs()
+	{
+		var files = new List<FileClass>();
+		string path = $"{_webHostEnvironment.WebRootPath}\\files\\";
+		var nFileId = 1;
+		foreach (var pdfPath in Directory.EnumerateFiles(path, "*.pdf"))
+		{
+			files.Add(new FileClass()
+			{
+				FileId = nFileId++,
+				Name = Path.GetFileName(pdfPath),
+				Path = pdfPath,
+			});
+		}
+		return files;
+	}
+
+    public async Task Upload(IBrowserFile file)
+	{
+		var path = Path.Combine(_webHostEnvironment.WebRootPath, "files", file.Name);
+		var memoryStream = new MemoryStream();
+		await file.OpenReadStream(10485760).CopyToAsync(memoryStream);
+		using (var fileStream = new FileStream(path, FileMode.Create, FileAccess.Write))
+		{
+			memoryStream.WriteTo(fileStream);
+		}
+		memoryStream.Close();
+	}
 }
