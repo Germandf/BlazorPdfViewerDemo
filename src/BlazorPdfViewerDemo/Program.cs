d101e5e01b9ef994f2e0a76763e537b8843e8405
germandefrancesco <germandefrancesco@hotmail.com>
2022-08-01 12:25:07 -0300

    SyncfusionDemo implemented

--- src/BlazorPdfViewerDemo/Program.cs
+++ src/BlazorPdfViewerDemo/Program.cs
@@ -1,9 +1,11 @@
 using BlazorPdfViewerDemo.Services;
+using Syncfusion.Blazor;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddScoped<IFileService, FileService>();
+builder.Services.AddSyncfusionBlazor();
 var app = builder.Build();
 if (!app.Environment.IsDevelopment())
     app.UseHsts();
