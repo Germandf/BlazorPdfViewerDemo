337217c4a807c90aede5f6011c042589cdee6089
germandefrancesco <germandefrancesco@hotmail.com>
2022-11-17 15:14:31 -0300

    Remove unused languages, tools and set default language

--- src/BlazorPdfViewerDemo/wwwroot/js/pdf.js
+++ src/BlazorPdfViewerDemo/wwwroot/js/pdf.js
@@ -1,4 +1,4 @@
-window.initWebViewer = (key, pdfUrl) => {
+window.initWebViewer = (key, pdfUrl, error) => {
     WebViewer({
         path: 'lib',
         licenseKey: key,
@@ -7,7 +7,15 @@ window.initWebViewer = (key, pdfUrl) => {
         instance.UI.disableElements(['selectToolButton']);
         instance.UI.disableElements(['viewControlsButton']);
         instance.UI.disableElements(['panToolButton']);
+        instance.UI.disableElements(['searchButton']);
+        instance.UI.disableElements(['thumbnailsSizeSlider']);
+        instance.UI.disableElements(['thumbnailsPanelButton']);
+        instance.UI.disableElements(['leftPanelTabs']);
+        instance.UI.setLanguage('es');
         instance.UI.setToolMode('Pan');
+        instance.UI.iframeWindow.addEventListener('loaderror', (err) => {
+            instance.showErrorMessage(error);
+        });
         const { documentViewer } = instance.Core;
         documentViewer.addEventListener('toolModeUpdated', () => {
             instance.UI.setToolMode('Pan');
