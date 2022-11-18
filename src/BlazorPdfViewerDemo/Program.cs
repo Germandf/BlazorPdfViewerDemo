78f5b1ad4bbdcb164622cb7386e3a52658a1725c
germandefrancesco <germandefrancesco@hotmail.com>
2022-11-18 11:43:29 -0300

    Fix .properties files with pdf js on production environments

--- src/BlazorPdfViewerDemo/Program.cs
+++ src/BlazorPdfViewerDemo/Program.cs
@@ -1,16 +1,28 @@
 using BlazorPdfViewerDemo.Services;
+using Microsoft.AspNetCore.StaticFiles;
 using Syncfusion.Blazor;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddScoped<IFileService, FileService>();
 builder.Services.AddSyncfusionBlazor();
+
 var app = builder.Build();
+
 if (!app.Environment.IsDevelopment())
     app.UseHsts();
+
 app.UseHttpsRedirection();
-app.UseStaticFiles();
+
+var provider = new FileExtensionContentTypeProvider();
+provider.Mappings.Add(".properties", "application/octet-stream");
+app.UseStaticFiles(new StaticFileOptions // needed for pdf js .properties files
+{
+    ContentTypeProvider = provider
+});
+
 app.UseRouting();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
